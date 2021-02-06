6d610b9af13d3ecf7e83af7d1b4a6b1e46723920
ybingc <yabing.bi@ngc.com>
2021-02-06 15:52:45 +0000

    Add logic and test

---
+++ src/main/java/com/simplilearn/lep/LepApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.lep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/lep/LepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.lep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
